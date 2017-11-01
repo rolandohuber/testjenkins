9e81beed53f2343a61a4fd7a8c064c1446d48447
rolandohuber <rolandohuber@hotmail.com>
2017-11-01 12:45:02 -0300

    dSaSasAASFASD

---
+++ src/testjenkins2/Test.java
@@ -0,0 +1,10 @@
+package testjenkins2;
+
+public class Test {
+	public static void main(String[] args) {
+		Integer lala= new Integer(9);
+		if(lala==9)
+			System.out.println("igual");
+		Integer pepe;
+	}
+}
